b2364de53f73bc46758dec20d727d32fa569a22e
JaeHeyK <jaeheyk@gmail.com>
2023-06-12 10:43:43 +0900

    Add initial package preparing script

---
+++ scripts/prepare_shell_ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt install build-essential git vim -y
+sudo apt install zsh -y
+chsh -s `which zsh`
+exec zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+~/bin/chezmoi apply ~/.zshrc
